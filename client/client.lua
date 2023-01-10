8ac48fe6aca5c0c8c51be8e926ebdb67398889a5
MeetxDev <102921640+MeetxDev@users.noreply.github.com>
2023-01-10 12:22:22 +0530

    Add files via upload

--- client/client.lua
+++ client/client.lua
@@ -1,4 +1,17 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local function DrawText3D(x, y, z, text)
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(x,y,z, 0)
+    DrawText(0.0, 0.0)
+    local factor = (string.len(text)) / 370
+    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
+    ClearDrawOrigin()
+end
 
 RegisterNetEvent('mxd-lift:start', function()
     for k, coords in pairs(Config.Locations["mxdlift"]) do
@@ -29,22 +42,40 @@ RegisterNetEvent('mxd-lift:start', function()
     end
 end)
 
-for k, coords in pairs(Config.Locations["mxdlift"]) do
-    exports['qb-target']:AddBoxZone("mxdlift"..k, vector3(coords.from.x, coords.from.y, coords.from.z), 2.0, 2.0, {
-		name = "mxdlift"..k,
-		heading = coords.from.w,
-		debugPoly = false,
-		minZ = coords.from.z-1,
-		maxZ = coords.from.z+1,
-	}, {
-		options = {
-			{
-				type = "client",
-				event = "mxd-lift:start",
-				icon = "fas fa-elevator",
-				label = coords.label,
+if Config.usetarget then
+	for k, coords in pairs(Config.Locations["mxdlift"]) do
+		exports['qb-target']:AddBoxZone("mxdlift"..k, vector3(coords.from.x, coords.from.y, coords.from.z), 2.0, 2.0, {
+			name = "mxdlift"..k,
+			heading = coords.from.w,
+			debugPoly = false,
+			minZ = coords.from.z-1,
+			maxZ = coords.from.z+1,
+		}, {
+			options = {
+				{
+					type = "client",
+					event = "mxd-lift:start",
+					icon = "fas fa-elevator",
+					label = coords.label,
+				},
 			},
-		},
-		distance = 2.5
-	})
+			distance = 2.5
+		})
+	end
+else
+	CreateThread(function()
+		while true do
+			for k, coords in pairs(Config.Locations["mxdlift"]) do
+				local pos = GetEntityCoords(PlayerPedId())
+				local dist = #(pos - vector3(coords.from.x, coords.from.y, coords.from.z))
+				if dist < 1.5 then
+					DrawText3D(coords.from.x, coords.from.y, coords.from.z, "~g~E~w~ - "..coords.label)
+				end
+				if IsControlJustReleased(0, 38) then
+					TriggerEvent("mxd-lift:start")
+				end
+			end
+			Wait(5)
+		end
+	end)
 end
--- config.lua
+++ config.lua
@@ -2,9 +2,10 @@ Config = {}
 
 Config.Locations = {
     ["mxdlift"] = {
-        [1] = {from = vector4(0, 0, 0, 0), to = vector4(0, 0, 0, 0), label = "Go Up"},
+        [1] = {from = vector4(0, 0, 0, 0), to = vector4(0, 0, 0, 0), label = "You Label"},
         --[2] = {from = vector4(0, 0, 0, 0), to = vector4(0, 0, 0, 0), label = "Go Down"},
     },
 }
 
 Config.playsound = true
+Config.usetarget = true
